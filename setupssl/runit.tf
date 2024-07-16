2635a0326362a9920374d08fea3a7b7a1dd1725a
Mitchell Lewars <54863058+malewars@users.noreply.github.com>
2024-07-16 15:55:40 -0400

    Update runit.tf

--- setupssl/runit.tf
+++ setupssl/runit.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "PowerShellScriptRunAlways" {
    
     provisioner "local-exec" {
-        command = "ECHO Y | .'${path.module}\\helpers\\ssl.ps1' -First 10"
+        command = "ECHO Y | .'${path.module}\\helpers\\ssl.ps1' "
         interpreter = ["PowerShell", "-Command"]
     }
 }
